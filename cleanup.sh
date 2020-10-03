8a92ef705e3eddaa3e745a219a7aa5029c325d3f
Robert Sæther <robsat91@gmail.com>
2020-10-03 03:55:10 +0200

    Fixed deploy scripts to work with composer

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,6 @@ rm -rf .phar 2> /dev/null
 rm -rf README.md 2> /dev/null
 rm -rf assets/src 2> /dev/null
 rm -rf node_modules 2> /dev/null
-rm -rf vendor 2> /dev/null
 rm -rf .babelrc 2> /dev/null
 rm -rf .eslintignore 2> /dev/null
 rm -rf .eslintrc 2> /dev/null
